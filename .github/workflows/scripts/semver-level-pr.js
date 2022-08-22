7e635a89da63f16bb0465074d9f963d2882debf3
Cleison Stein <cscleison@gmail.com>
2022-08-22 20:00:51 +0100

    Refactor

--- .github/workflows/scripts/semver-level-pr.js
+++ .github/workflows/scripts/semver-level-pr.js
@@ -3,7 +3,7 @@ module.exports = async ({ github, context, core }) => {
   const defaultLevel = "patch";
 
   function extractPrNumber(commitMsg) {
-    const squashRegex = /\(#(\d+)\)/;
+    const squashRegex = /\(#(\d+)\)$/;
     const squashMatches = commitMsg.match(squashRegex);
 
     if (squashMatches?.length > 1) {
