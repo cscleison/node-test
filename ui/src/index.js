16919adb36429275f025ae5d064b76d5a42f54e6
Cleison Stein <cscleison@gmail.com>
2022-08-25 13:52:22 +0100

    New change

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 80;
 
+// change
+
 app.get("/", (req, res) => res.send("Hello, World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
