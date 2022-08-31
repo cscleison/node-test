242c295e8a596fb31a4b742e0db8691959eafed5
Cleison Stein <cscleison@gmail.com>
2022-08-31 14:33:18 +0100

    test

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 80;
 
+// test
+
 app.get("/health", (req, res) => res.sendStatus(200));
 
 app.get("/", (req, res) => res.send("Hello, World!"));
